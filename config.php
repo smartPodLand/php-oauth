359cd4fcd216fa2457a45da1a399d6d368b212ac
Mehran Rahbardar <mrahbardar@users.noreply.github.com>
2018-04-08 16:13:54 +0430

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $config = [
         //main endpont:
         "service"=>"[sso-address]",
         //oauth endpoint:
-        "sso"=>"[sso-address]/oauth__",
+        "sso"=>"[sso-address]/oauth2/",
         "client_id"=>"__CLIENT_ID__",
         "client_secret"=>"__CLIENT_SECRET__",
         //the url of where this app is deployed
